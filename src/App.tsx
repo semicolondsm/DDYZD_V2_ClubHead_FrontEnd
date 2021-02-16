bcc8c338fc121941cff36bcc56c1baca49074c07
eungyeole <eungyeole@naver.com>
2021-02-17 03:59:51 +0900

    UPDATE ; Router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Callback, { onSilentRefresh } from './components/Auth/Callback/Callback';
 import Login from './components/Auth/Login/Login';
+import ClubList from './components/ClubList/ClubList';
 import Header from './components/Header/Header';
 
 function App() {
@@ -10,9 +11,10 @@ function App() {
   },[])
   return (
     <Router>
-      <Route exact path="/" component={Header}></Route>
+      <Route exact path="/header" component={Header}></Route>
       <Route exact path="/callback" component={Callback}></Route>
       <Route exact path="/login" component={Login}></Route>
+      <Route exact path="/" component={ClubList}></Route>
     </Router>
   );
 }
