8e83526ff25fc2f657d7486cf972973f33c2b125
eungyeole <eungyeole@naver.com>
2021-02-17 00:35:26 +0900

    CREATE ; Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import * as S from "./styles"
+function Header(){
+    return(
+        <header>
+            <div></div>
+            <div></div>
+        </header>
+    )
+}
+export default Header;
