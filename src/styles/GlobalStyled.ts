6db49a1a5421bbfc3733e572e6782fcdbb0c2828
eungyeole <eungyeole@naver.com>
2021-02-17 02:02:34 +0900

    CREATE ; GlobalStyled

---
+++ src/styles/GlobalStyled.ts
@@ -0,0 +1,9 @@
+import styled, { createGlobalStyle } from "styled-components"
+export default createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        text-decoration: none;
+    }
+`
