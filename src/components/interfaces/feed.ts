f5683e7e327eefe10b1cb9b681d9fcab0d510453
eungyeole <eungyeole@naver.com>
2021-02-17 06:32:46 +0900

    CREATE ; FeedInterface

---
+++ src/components/interfaces/feed.ts
@@ -0,0 +1,13 @@
+export default interface FeedData{
+    clubName : string,
+    content : string,
+    feedId : number,
+    flag : boolean,
+    flags : number,
+    follow : boolean,
+    media : string[],
+    owner : boolean,
+    pin : boolean,
+    profileImage : string,
+    uploadAt : Date
+}
