9d608cec9640b17ea7f4579a60c488d1803ad372
eungyeole <eungyeole@naver.com>
2021-02-17 02:02:45 +0900

    CREATE ; Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,18 @@
+import { Link } from "react-router-dom";
 import * as S from "./styles"
 function Header(){
     return(
-        <header>
-            <div></div>
-            <div></div>
-        </header>
+        <S.HeaderWrapper>
+            <S.LogoWrapper>
+                <Link to="/">
+                    <S.Logo/>
+                    <h3>대동여지도</h3>
+                </Link>
+            </S.LogoWrapper>
+            <S.MenuWrapper>
+                <li onClick={()=>window.location.href="/login"}>로그인</li>
+            </S.MenuWrapper>
+        </S.HeaderWrapper>
     )
 }
 export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,1 +1,33 @@
 import styled from "styled-components"
+import { ReactComponent as LogoIco } from "../../assets/images/logo.svg"
+
+export const HeaderWrapper=styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 10px 300px;
+
+`
+export const LogoWrapper=styled.div`
+    cursor: pointer;
+    & a{
+        color: black;
+        display: flex;
+        align-items: center;
+    }
+
+`
+export const MenuWrapper=styled.ul`
+    display: flex;
+    align-items: center;
+    & li {
+        cursor: pointer;
+    }
+`
+
+
+
+export const Logo = styled(LogoIco)`
+    width: 40px;
+    height: 40px;
+`
