0c5a2e706f3d944990078b12b4265d0a34f225ec
eungyeole <eungyeole@naver.com>
2021-02-17 04:01:27 +0900

    UPDATE ; Header get UserData

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,34 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import UserContext from "../../utils/context/user";
 import * as S from "./styles"
 function Header(){
+    const { user_state } = useContext(UserContext)
+    function Logout(){
+        localStorage.clear();
+        window.location.href="/"
+    }
     return(
-        <S.HeaderWrapper>
-            <S.LogoWrapper>
-                <Link to="/">
-                    <S.Logo/>
-                    <h3>대동여지도</h3>
-                </Link>
-            </S.LogoWrapper>
-            <S.MenuWrapper>
-                <li onClick={()=>window.location.href="/login"}>로그인</li>
-            </S.MenuWrapper>
-        </S.HeaderWrapper>
+        <div style={{width: "100%"}}>
+            <S.HeaderWrapper>
+                <S.LogoWrapper>
+                    <Link to="/">
+                        <S.Logo/>
+                        <h3>대동여지도</h3>
+                    </Link>
+                </S.LogoWrapper>
+                <S.MenuWrapper>
+                    {
+                        user_state ? 
+                            <>
+                                <li>{user_state.name}님</li>
+                                <li onClick={Logout}>로그아웃</li>
+                            </>
+                        : <li onClick={()=>window.location.href="/login"}>로그인</li>
+                    }
+                </S.MenuWrapper>
+            </S.HeaderWrapper>
+        </div>
     )
 }
 export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,11 +1,13 @@
 import styled from "styled-components"
 import { ReactComponent as LogoIco } from "../../assets/images/logo.svg"
 
-export const HeaderWrapper=styled.div`
+export const HeaderWrapper=styled.header`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    margin: 10px 300px;
+    margin: 0 auto;
+    max-width: 1080px;
+    height: 55px;
 
 `
 export const LogoWrapper=styled.div`
@@ -13,6 +15,7 @@ export const LogoWrapper=styled.div`
     & a{
         color: black;
         display: flex;
+        font-size: 14px;
         align-items: center;
     }
 
@@ -21,7 +24,22 @@ export const MenuWrapper=styled.ul`
     display: flex;
     align-items: center;
     & li {
+        margin-left: 15px;
+        position: relative;
         cursor: pointer;
+        font-size: 14px;
+        &::before{
+            content: "";
+            display: inline-block;
+            width: 1px;
+            height: 15px;
+            background: #c8c8c8;
+            position: absolute;
+            left: -7.5px;
+        }
+        &:nth-child(1)::before{
+            display: none;
+        }
     }
 `
 
