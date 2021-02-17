098ed14e461c3d29a7148648648166ea0295fe01
eungyeole <eungyeole@naver.com>
2021-02-17 22:14:42 +0900

    UPDATE ; FeedModals

--- src/components/Modals/Feed/Feed.tsx
+++ src/components/Modals/Feed/Feed.tsx
@@ -1,5 +1,34 @@
+import { useContext, useEffect, useState } from "react";
+import club from "../../../utils/api/club";
+import ModalContext from "../../../utils/context/modals";
 import * as S from "./styles"
-function Feed(){
-
+function Feed({club_id} : {club_id : number}){
+    const [state,setState] = useState(false);
+    const [content, setContent] = useState("");
+    const { setModalState } = useContext(ModalContext)
+    function checkHandler(e : any){
+        setState(e.target.checked)
+    }
+    function onSubmit(){
+        club.addFeed(club_id, content, state)
+        .then((res)=>{
+            setContent("");
+            window.location.href=`/club/${club_id}`
+        })
+    }
+    useEffect(()=>{
+        console.log(club_id);
+    },[])
+    return(
+        <S.FeedWrapper>
+            <h3>게시물 만들기</h3>
+            <textarea onChange={(e)=>setContent(e.target.value)} placeholder="이곳을 눌러 새로운 게시물을 등록해보세요."></textarea>
+            <label>
+                <input onChange={checkHandler} type="checkbox"></input>
+                <p>고정</p>
+            </label>
+            <button onClick={()=>{ onSubmit(); setModalState(null);}}>게시</button>
+        </S.FeedWrapper>
+    )
 }
 export default Feed;
