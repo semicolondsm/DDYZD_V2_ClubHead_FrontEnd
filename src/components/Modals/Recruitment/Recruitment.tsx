7bf8976bfed42377247e2d1d62bc03e3ed2254b1
eungyeole <eungyeole@naver.com>
2021-02-17 22:12:53 +0900

    UPDATE ; Recruitment

--- src/components/Modals/Recruitment/Recruitment.tsx
+++ src/components/Modals/Recruitment/Recruitment.tsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from "react";
+import club from "../../../utils/api/club";
 import * as S from "./styles"
-function Recruitment(){
-
+function Recruitment({club_id} : {club_id : number}){
+    const [date, setDate] = useState<Date | null>(null);
+    function onSubmit(){
+        date && club.addRecru(club_id, date, ["프론트엔드","백엔드"])
+    }
+    return(
+        <S.Wrapper>
+            <h3>모집 공고</h3>
+            <input onChange={(e)=>setDate(e.target.valueAsDate)} type='date' name='userBirthday'/>
+            <button onClick={onSubmit}>게시</button>
+        </S.Wrapper>
+    )
 }
 export default Recruitment;
--- src/components/Modals/Recruitment/styles.ts
+++ src/components/Modals/Recruitment/styles.ts
@@ -1,1 +1,28 @@
 import styled from "styled-components"
+export const Wrapper=styled.div`
+    position: relative;
+    z-index: 9999;
+    background: white;
+    padding: 0 15px;
+    border-radius: 10px;
+    width: 450px;
+    display: flex;
+    flex-direction: column;
+    & h3{
+        border-bottom: 1px solid #0000001a;
+        font-weight: 700;
+        font-size: 22px;
+        text-align: center;
+        padding: 15px;
+        margin-bottom: 10px;
+    }
+    & button{
+        border: none;
+        background: #350871;
+        color: white;
+        padding: 10px;
+        border-radius: 5px;
+        cursor: pointer;
+        margin-bottom: 20px;
+    }
+`
