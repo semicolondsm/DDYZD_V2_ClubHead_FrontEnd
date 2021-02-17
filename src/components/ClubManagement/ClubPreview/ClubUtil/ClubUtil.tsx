ee5c4a747ecfb24194730fe064dba7c9e3058bbd
eungyeole <eungyeole@naver.com>
2021-02-17 22:14:20 +0900

    UPDATE ; ModalState

--- src/components/ClubManagement/ClubPreview/ClubUtil/ClubUtil.tsx
+++ src/components/ClubManagement/ClubPreview/ClubUtil/ClubUtil.tsx
@@ -1,10 +1,13 @@
+import { useContext } from "react"
+import ModalContext from "../../../../utils/context/modals"
 import * as S from "./styles"
-function ClubUtil(){
+function ClubUtil({club_id} : {club_id : number}){
+    const { setModalState } = useContext(ModalContext)
     return(
         <S.Wrapper>
             <S.UtilWrapper>
-                <li><S.feedIco/><p>글쓰기</p></li>
-                <li><S.recruitmentIco/><p>모집공고</p></li>
+                <li onClick={()=>setModalState({state : "feed", club_id : club_id})}><S.feedIco/><p>글쓰기</p></li>
+                <li onClick={()=>setModalState({state : "recruitment", club_id : club_id})}><S.recruitmentIco/><p>모집공고</p></li>
             </S.UtilWrapper>
         </S.Wrapper>
     )
