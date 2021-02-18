ab9780441a4532d0cd0c01c31c746202af3ef7f1
eungyeole <eungyeole@naver.com>
2021-02-19 06:46:34 +0900

    UPDATE ; Modals

--- src/components/Modals/Modals.tsx
+++ src/components/Modals/Modals.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import ModalContext from "../../utils/context/modals";
 import Banner from "./Banner/Banner";
 import Feed from "./Feed/Feed";
+import FeedMenu from "./FeedMenu/FeedMenu";
 import Profile from "./Profile/Profile";
 import Recruitment from "./Recruitment/Recruitment";
 import * as S from "./styles"
@@ -16,11 +17,13 @@ function Modals(){
                             modal_state.state==="feed" ?
                                 <Feed club_id={modal_state.club_id}></Feed>
                             : modal_state.state==="banner" ?
-                                <Banner></Banner>
+                                <Banner club_id={modal_state.club_id}></Banner>
                             : modal_state.state==="profile" ?
-                                <Profile></Profile> 
+                                <Profile club_id={modal_state.club_id}></Profile> 
                             : modal_state.state==="recruitment" ?
                                 <Recruitment club_id={modal_state.club_id}></Recruitment>
+                            : modal_state.state==="feedmenu" ? 
+                                <FeedMenu owner={modal_state.owner} feed_id={modal_state.feed_id}></FeedMenu>
                             : null
                         }
                         <S.Overlay onClick={()=>setModalState(null)}></S.Overlay>
