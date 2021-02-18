9fa242541abf94ccab818ffb358db3d1b60f1254
eungyeole <eungyeole@naver.com>
2021-02-19 03:12:10 +0900

    UPDATE ; Recruitment Majors

--- src/components/Modals/Recruitment/Recruitment.tsx
+++ src/components/Modals/Recruitment/Recruitment.tsx
@@ -1,15 +1,54 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import club from "../../../utils/api/club";
+import ModalContext from "../../../utils/context/modals";
 import * as S from "./styles"
 function Recruitment({club_id} : {club_id : number}){
     const [date, setDate] = useState<Date | null>(null);
+    const exMajors = ["프론트엔드","백엔드","Android","IOS","인공지능" ];
+    const [majors,setMajors]=useState<string[]>([]);
+    const [value,setValue] = useState<string>("");
+    const { setModalState } = useContext(ModalContext)
     function onSubmit(){
-        date && club.addRecru(club_id, date, ["프론트엔드","백엔드"])
+        date && club.addRecru(club_id, date, majors)
+        .then((res)=>{
+            window.location.href=`/club/${club_id}`
+            setModalState(null);
+        })
+        .catch((e)=>alert(e))
+    }
+    function delMajors(index : number){
+        let temp=[...majors];
+        temp.splice(index,1);
+        setMajors(temp);
+    }
+    function addMajors(index : number){
+        setMajors([...majors, exMajors[index]])
+    }
+    function keyDownHandler(e: any){
+        if(e.code==="Enter"){
+            if(value.length>0 && value.length<7){
+                setMajors([...majors,value]);
+                setValue("")
+            }
+        }
     }
     return(
         <S.Wrapper>
             <h3>모집 공고</h3>
-            <input onChange={(e)=>setDate(e.target.valueAsDate)} type='date' name='userBirthday'/>
+            <p>모집분야</p>
+            <S.majorInput onChange={(e)=>setValue(e.target.value)} value={value} onKeyDown={keyDownHandler} placeholder="모집분야를 입력해주세요." type="type"></S.majorInput>
+            <S.exmajorList>
+                {
+                    exMajors.map((i,index)=>(<span onClick={()=>addMajors(index)} key={index}>{i}</span>))
+                }
+            </S.exmajorList>
+            <S.majorList>
+                {
+                    majors.map((i,index)=>(<span onClick={()=>delMajors(index)} key={index}>{i}</span>))
+                }
+            </S.majorList>
+            <p>종료일</p>
+            <input style={{marginBottom: "30px"}} onChange={(e)=>setDate(e.target.valueAsDate)} type='date' name='userBirthday'/>
             <button onClick={onSubmit}>게시</button>
         </S.Wrapper>
     )
--- src/components/Modals/Recruitment/styles.ts
+++ src/components/Modals/Recruitment/styles.ts
@@ -16,8 +16,13 @@ export const Wrapper=styled.div`
         padding: 15px;
         margin-bottom: 10px;
     }
+    & input {
+        border: none;
+        margin: 10px 0;
+    }
     & button{
         border: none;
+        border: 1px solid #350871;
         background: #350871;
         color: white;
         padding: 10px;
@@ -25,4 +30,37 @@ export const Wrapper=styled.div`
         cursor: pointer;
         margin-bottom: 20px;
     }
+`
+export const exmajorList=styled.div`
+    & span{
+        padding: 4px 15px;
+        border-radius: 30px;
+        font-size: 12px;
+        background: #eee;
+        color: #767676;
+        margin-right: 5px;
+        cursor: pointer;
+    }
+`
+export const majorList=styled.div`
+    margin-top: 10px;
+    margin-bottom: 30px;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    & span{
+        padding: 4px 15px;
+        border-radius: 30px;
+        font-size: 12px;
+        background: #2ebd59;
+        color: white;
+        margin-right: 5px;
+        margin-bottom: 5px;
+        cursor: pointer;
+    }
+`
+export const majorInput=styled.input`
+    border: 1px solid #d9dfeb !important;
+    padding: 10px 10px;
+    border-radius: 5px;
 `
