4601bc0c3fbf6dd4ed09bd6bbfe2a00431e68bcf
eungyeole <eungyeole@naver.com>
2021-02-19 06:46:42 +0900

    UPDATE ; Profile Modal

--- src/components/Modals/Profile/Profile.tsx
+++ src/components/Modals/Profile/Profile.tsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+import club from "../../../utils/api/club";
 import * as S from "./styles"
-function Profile(){
+function Profile({club_id} : {club_id : number}){
+    const [file, setFile] = useState<File>();
+    const [preview,setPreview] = useState<string | ArrayBuffer>("");
+    function fileHandler(e : any){
+        console.log(e.target.files[0])
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            reader.result && setPreview(reader.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+        setFile(e.target.files[0])
+    }
+    function onSubmit(e : any){
+        const fd=new FormData();
+        file && fd.append("file", file)
+        club.setProfile(club_id, fd)
+        .then((res)=>window.location.href=window.location.href)
+        .catch((e)=>e.response.status===403 && alert("클럽장이 아닙니다."))
+    }
     return(
-        <div></div>
+        <S.Wrapper>
+            <h3>프로필 사진 변경</h3>
+            <input onChange={fileHandler} type="file"></input>
+            {
+                typeof(preview)==="string" ?
+                    <img src={preview}></img>
+                : null
+            }
+            <button onClick={onSubmit}>프로필 사진 변경</button>
+        </S.Wrapper>
     )
 }
 export default Profile;
