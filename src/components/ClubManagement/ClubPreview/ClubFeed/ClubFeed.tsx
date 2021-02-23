a3dfbf3c913e9b26d2933bf1d57d967bb69b6c1a
eungyeole <eungyeole@naver.com>
2021-02-23 14:30:51 +0900

    UPDATE ; ClubFeed infiniteScroll

--- src/components/ClubManagement/ClubPreview/ClubFeed/ClubFeed.tsx
+++ src/components/ClubManagement/ClubPreview/ClubFeed/ClubFeed.tsx
@@ -4,13 +4,40 @@ import FeedData from "../../../interfaces/feed";
 import FeedCard from "./FeedCard/FeedCard";
 import * as S from "./styles"
 function ClubFeed({club_id} : {club_id : number}){
-    const [data, setData] = useState<FeedData[]>();
+    const [data, setData] = useState<FeedData[]>([]);
+    const [page, setPage] = useState(0);
+    const [last, setLast] = useState(false);
+    function infiniteScroll(){
+        let scrollHeight = Math.max(
+          document.documentElement.scrollHeight,
+          document.body.scrollHeight  
+        );
+        let scrollTop = Math.max(
+          document.documentElement.scrollTop,
+          document.body.scrollTop
+        );
+        let clientHeight = document.documentElement.clientHeight;
+      
+        if (scrollTop + clientHeight >= scrollHeight-200) {  
+          club.getFeed(club_id,page+1)
+          .then((res)=>{
+            setPage(page+1);
+            setData([...data , ...res.data]);
+            if(res.data.length===0){
+              setLast(true)
+            }
+          })
+          window.onscroll=null;
+          
+        }
+      }
     useEffect(()=>{
-        club.getFeed(club_id, 0)
+        club.getFeed(club_id, page)
         .then((res)=>setData(res.data))
+        .catch((e)=>console.log(e))
     },[])
     useEffect(()=>{
-        console.log(data);
+      if(!last) window.onscroll=infiniteScroll;
     },[data])
     return(
         <S.FeedList>
