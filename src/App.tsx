9c44cf9287cdf52f848ccf813d2a1899febd854d
eungyeole <eungyeole@naver.com>
2021-02-24 14:36:33 +0900

    UPDATE ; Routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Callback, { onSilentRefresh } from './components/Auth/Callback/Callback';
 import Login from './components/Auth/Login/Login';
+import Chat from './components/Chat/Chat';
 import ClubList from './components/ClubList/ClubList';
 import ClubManagement from './components/ClubManagement/ClubManagement';
 import ClubMemebers from './components/ClubMembers/ClubMembers';
@@ -17,8 +18,8 @@ function App() {
       <Route exact path="/callback" component={Callback}></Route>
       <Route exact path="/login" component={Login}></Route>
       <Route exact path="/" component={ClubList}></Route>
-      <Route exact path="/club/:id/members" component={ClubMemebers}></Route>
-      <Route exact path="/club/:id" component={ClubManagement}></Route> 
+      <Route exact path="/club/:id/chat" component={Chat}></Route>
+      <Route exact path="/club/:id" component={ClubManagement}></Route>
     </Router>
   );
 }
