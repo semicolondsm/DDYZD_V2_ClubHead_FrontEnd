07df44f4b140507359ba8fd2cae5b03ad3bf389e
eungyeole <eungyeole@naver.com>
2021-02-27 21:09:05 +0900

    UPDATE ; Chats

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,16 +1,31 @@
+import { useEffect, useState } from "react";
+import chat from "../../utils/api/chat";
 import Header from "../Header/Header";
 import ChatBreakDown from "./ChatBreakDown/ChatBreakDown";
 import ChatRooms from "./ChatRooms/ChatRooms";
 import ChatStatusBoard from "./ChatStatusBoard/ChatStatusBoard";
 import * as S from "./styles"
-function Chat(){
+function Chat({match: { params }} : { match : any}){
+    const [room_token, setRoomToken] = useState<string | null>(null);
+    useEffect(()=>{
+        setRoomToken(null);
+        params.chatId && 
+        chat.getToken(params.chatId)
+        .then((res)=>{
+            setRoomToken(res.data.room_token);
+        })
+    },[params])
     return(
         <>
             <Header></Header>
             <S.Wrapper>
-                <ChatStatusBoard></ChatStatusBoard>
-                <ChatRooms></ChatRooms>
-                <ChatBreakDown></ChatBreakDown>
+                <ChatStatusBoard club_id={params.id}></ChatStatusBoard>
+                <ChatRooms club_id={params.id}></ChatRooms>
+                {
+                    room_token ? 
+                        <ChatBreakDown roomToken={room_token} chatId={params.chatId}></ChatBreakDown>
+                    : null
+                }
             </S.Wrapper>
         </>
     )
