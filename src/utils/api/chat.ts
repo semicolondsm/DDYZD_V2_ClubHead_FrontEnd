53c28d769d5cfdb28eca557f8c44add99d35fc32
eungyeole <eungyeole@naver.com>
2021-02-27 21:15:29 +0900

    UPDATE ; ChatApis

--- src/utils/api/chat.ts
+++ src/utils/api/chat.ts
@@ -1,8 +1,38 @@
 import request from '../axios'
 export default{
-    getChatList(){
+    getUserList(club_id : number){
         return request({
-            url : '/chat/list',
+            url : `/chat/list`,
+            method : 'get',
+            params : {
+                club_id : club_id
+            },
+            headers : {
+                "Authorization" : `Bearer ${localStorage.accessToken}`
+            }
+        })
+    },
+    getChatList(room_id : number){
+        return request({
+            url : `/chat/${room_id}/breakdown`,
+            method : 'get',
+            headers : {
+                "Authorization" : `Bearer ${localStorage.accessToken}`
+            }
+        })
+    },
+    getRoomInfo(room_id : number){
+        return request({
+            url : `/room/${room_id}/info`,
+            method : 'get',
+            headers : {
+                "Authorization" : `Bearer ${localStorage.accessToken}`
+            },
+        })
+    },
+    getToken(room_id : number){
+        return request({
+            url : `/room/${room_id}/token`,
             method : 'get',
             headers : {
                 "Authorization" : `Bearer ${localStorage.accessToken}`
