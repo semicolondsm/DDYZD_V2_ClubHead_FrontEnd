c3567040f98b8d02be1a60b669188d9fda2b5d46
eungyeole <eungyeole@naver.com>
2021-03-02 20:04:54 +0900

    UPDATE ; RedirectURL

--- src/components/Auth/Login/Login.tsx
+++ src/components/Auth/Login/Login.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-const Oauthurl="https://developer.dsmkr.com/external/login?redirect_url=https://ddyzd.dsmkr.com/callback&client_id=3b11a5ab276b40a79708f719ee1eb6cb"
+const Oauthurl="https://developer.dsmkr.com/external/login?redirect_url=https://ddyzd.dsmkr.com/callback&client_id=ab840667ddcd41dc81b29f8f128a0e66"
 function Login(){
     useEffect(()=>{
         window.location.href=Oauthurl;
