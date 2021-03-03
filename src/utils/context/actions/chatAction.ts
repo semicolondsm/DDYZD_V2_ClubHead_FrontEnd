af109a4019897119f1e2625d6fe9e74cfd02330c
Sung Ye In <syi9397@naver.com>
2021-03-03 21:59:25 +0900

    UPDATE; chat action

--- src/utils/context/actions/chatAction.ts
+++ src/utils/context/actions/chatAction.ts
@@ -12,8 +12,36 @@ import {
 
 import { useChatState, useChatDispatch } from "../chatProvider";
 
-export async function getApplicant() {}
+import chatApi from "../../api/chat";
 
-export async function getChatList() {}
+const dispatch: any = useChatDispatch();
 
-export async function getRoomList() {}
+export async function getApplicant(club_id: number) {
+  dispatch({ type: GET_APPLICANT_LIST });
+  try {
+    const response = await chatApi.getApplicant(club_id);
+    dispatch({ type: GET_APPLICANT_LIST_SUCCESS, data: response.data });
+  } catch (err) {
+    dispatch({ tpye: GET_APPLICANT_LIST_ERROR, error: err });
+  }
+}
+
+export async function getChatList(room_id: number) {
+  dispatch({ type: GET_CHAT_LIST });
+  try {
+    const response = await chatApi.getApplicant(room_id);
+    dispatch({ type: GET_CHAT_LIST_SUCCESS, data: response.data });
+  } catch (err) {
+    dispatch({ tpye: GET_CHAT_LIST_ERROR, error: err });
+  }
+}
+
+export async function getRoomList(club_id: number) {
+  dispatch({ type: GET_ROOM_LIST });
+  try {
+    const response = await chatApi.getUserList(club_id);
+    dispatch({ type: GET_ROOM_LIST_SUCCESS, data: response.data });
+  } catch (err) {
+    dispatch({ tpye: GET_ROOM_LIST_ERROR, error: err });
+  }
+}
