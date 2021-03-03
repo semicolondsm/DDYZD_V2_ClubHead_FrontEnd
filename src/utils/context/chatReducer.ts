51c638f029cce5f93690fd3f3c3366e67b34f8af
Sung Ye In <syi9397@naver.com>
2021-03-03 18:50:17 +0900

    CREATE; reducers

--- src/utils/context/chatReducer.ts
+++ src/utils/context/chatReducer.ts
@@ -1,6 +1,34 @@
+import {
+  GET_APPLICANT_LIST,
+  GET_APPLICANT_LIST_SUCCESS,
+  GET_APPLICANT_LIST_ERROR,
+  GET_CHAT_LIST,
+  GET_CHAT_LIST_SUCCESS,
+  GET_CHAT_LIST_ERROR,
+  GET_ROOM_LIST,
+  GET_ROOM_LIST_SUCCESS,
+  GET_ROOM_LIST_ERROR,
+} from "./types";
+
 export default function (state: any, action: any) {
   switch (action.type) {
-    case "get":
+    case GET_APPLICANT_LIST:
+      return { ...state };
+    case GET_APPLICANT_LIST_SUCCESS:
+      return { ...state };
+    case GET_APPLICANT_LIST_ERROR:
+      return { ...state };
+    case GET_CHAT_LIST:
+      return { ...state };
+    case GET_CHAT_LIST_SUCCESS:
+      return { ...state };
+    case GET_CHAT_LIST_ERROR:
+      return { ...state };
+    case GET_ROOM_LIST:
+      return { ...state };
+    case GET_ROOM_LIST_SUCCESS:
+      return { ...state };
+    case GET_ROOM_LIST_ERROR:
       return { ...state };
     default:
       throw new Error(`Unhanded action type: ${action.type}`);
